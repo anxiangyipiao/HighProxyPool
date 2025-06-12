229e2fb6de7948c996d2ce3a85adb1c3417271f8
anxiangyipiao <appleapp2017@outlook.com>
2025-06-12 17:16:36 +0800

    1

--- locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class ProxyAPITestUser(HttpUser):
-    wait_time = between(1, 5)  # 每个用户请求之间的等待时间（秒）
-
-    @task
-    def get_proxy(self):
-        self.client.get("/api/get_proxy")  # 测试你的 `/api/get_proxy` 路由
